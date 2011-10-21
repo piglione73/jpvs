f5055082555e7cadf0fd7e8a10c843c5304d819d
Roberto Rossi <roberto.rossi@paviasystem.com>
2011-10-21 19:34:54 +0200

    Version 0.0

---
+++ javascript/src/Event.js
@@ -0,0 +1,38 @@
+﻿jpvs.Event = function (widget) {
+    this.widget = widget;
+    this.handlers = {};
+};
+
+jpvs.Event.prototype.bind = function (handlerName, handler) {
+    if (!handler) {
+        handler = handlerName;
+        handlerName = new Date().toString();
+    }
+
+    this.handlers[handlerName] = handler;
+
+    return this.widget;
+};
+
+jpvs.Event.prototype.unbind = function (handlerName) {
+    delete this.handlers[handlerName];
+    return this.widget;
+};
+
+jpvs.Event.prototype.fire = function (widget, handlerName, params) {
+    if (handlerName)
+        fireHandler(this.handlers[handlerName], params);
+    else {
+        for (var hn in this.handlers) {
+            var h = this.handlers[hn];
+            fireHandler(h, params);
+        }
+    }
+
+    return this.widget;
+
+    function fireHandler(handler) {
+        if (handler)
+            handler.call(widget, params);
+    }
+};

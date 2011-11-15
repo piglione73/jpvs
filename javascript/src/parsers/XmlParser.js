4d7f8fd0cb3f054e2c3f8780503ff4607a9c5900
Roberto Rossi <roberto.rossi@paviasystem.com>
2011-11-15 19:31:14 +0100

    Version 0.0 - XML parser

---
+++ javascript/src/parsers/XmlParser.js
@@ -0,0 +1,198 @@
+﻿/* JPVS
+Module: parsers
+Classes: XmlParser
+Depends: 
+*/
+var XmlParser = (function () {
+
+    var OutOfTag = 0,
+        TagName = 1,
+        WaitForAttributeName = 2,
+        AttributeName = 3,
+        WaitForAttributeValue = 4,
+        AttributeValue = 5,
+        WaitForElementEnd = 6;
+
+
+    function createEmptyNode() {
+        return {
+            name: "",
+            attributes: {},
+            children: [],
+            value: null
+        };
+    }
+
+    function parseString(s) {
+        //No string --> return null
+        if (!s || s == "")
+            return null;
+
+        //String present, let's parse
+        var state = OutOfTag;
+        var nodeStack = [];
+        var curNode, curAttrName, curAttrValue;
+
+        for (var i = 0; i < s.length; i++) {
+            var c = s[i];
+
+            if (state == OutOfTag) {
+                //Tag not yet found, look for "<"
+                if (c == "<") {
+                    //Tag is starting, prepare for reading the tag name
+                    state = TagName;
+
+                    //Create a new node and add it as a child to the current node, if any
+                    var oldCurNode = curNode;
+                    curNode = createEmptyNode();
+                    nodeStack.push(curNode);
+
+                    if (oldCurNode)
+                        oldCurNode.children.push(curNode);
+                }
+                else {
+                    //Any other char is ignored
+                }
+            }
+            else if (state == TagName) {
+                //We are now reading the tag name
+                //Stop at the first blank or or "/" or ">"
+                if (c == " ") {
+                    //End of tag name, look for attributes
+                    state = WaitForAttributeName;
+                }
+                else if (c == "/") {
+                    if (curNode.name == "") {
+                        //This is a closing tag (</example>) and we have just read the slash, let's keep on reading the name
+                        curNode.closing = true;
+                    }
+                    else {
+                        //This is a slash after a tag name
+                        //End of tag, no children, let's wait for the closing ">"
+                        state = WaitForElementEnd;
+                    }
+                }
+                else if (c == ">") {
+                    //End of tag
+                    state = OutOfTag;
+
+                    //Let's see what to do with the node stack
+                    if (curNode.closing) {
+                        //Just finished reading a closing tag
+                        //Let's remove this and pop it from the stack
+                        if (nodeStack.length > 1) {
+                            nodeStack.pop();
+                            curNode = nodeStack[nodeStack.length - 1];
+
+                            //The closing tag itself is not a child of the parent: let's pop it off
+                            curNode.children.pop();
+                        }
+                    }
+                    else {
+                    }
+                }
+                else {
+                    //Still reading the tag name
+                    curNode.name += c;
+                }
+            }
+            else if (state == WaitForAttributeName) {
+                if (c == " ") {
+                    //Still waiting for attribute name
+                }
+                else if (c == "/") {
+                    //End of tag, no children
+                    state = WaitForElementEnd;
+                }
+                else if (c == ">") {
+                    //End of tag
+                    state = OutOfTag;
+                }
+                else {
+                    //Here an attribute name starts
+                    state = AttributeName;
+                    curAttrName = c;
+                    curAttrValue = "";
+                }
+            }
+            else if (state == AttributeName) {
+                if (c == "=") {
+                    //End of attribute name, wait for value
+                    state = WaitForAttributeValue;
+                }
+                else {
+                    //Still reading the attribute name
+                    curAttrName += c;
+                }
+            }
+            else if (state == WaitForAttributeValue) {
+                //Only " or blank is valid here
+                if (c == " ") {
+                    //Keep waiting
+                }
+                else if (c == "\"") {
+                    //Opened quote: now we prepare to read the value
+                    state = AttributeValue;
+                }
+                else {
+                    //Not valid
+                    throwInvalidChar();
+                }
+            }
+            else if (state == AttributeValue) {
+                //We are in the attribute value: keep reading until a closing quote
+                if (c == "\"") {
+                    //Closing quote, let's wait for a possible next attribute
+                    state = WaitForAttributeName;
+
+                    //Add the attribute name/value to the collection
+                    curNode.attributes[curAttrName] = curAttrValue;
+                }
+                else {
+                    //Part of value
+                    curAttrValue += c;
+                }
+            }
+            else if (state == WaitForElementEnd) {
+                //Found "/", so now the only acceptable char is > or blank
+                if (c == " ") {
+                    //Keep waiting
+                }
+                else if (c == ">") {
+                    //Tag closed
+                    state = OutOfTag;
+
+                    //This also closes the current node
+                    if (nodeStack.length > 1) {
+                        nodeStack.pop();
+                        curNode = nodeStack[nodeStack.length - 1];
+                    }
+                }
+                else {
+                    //Not valid
+                    throwInvalidChar();
+                }
+            }
+            else
+                throwUnexpectedState();
+        }
+
+        //End
+        return nodeStack[0] || null;
+
+
+        //Utilities for compacting the minified script
+        function throwInvalidChar() {
+            throw "Invalid character: " + c;
+        }
+
+        function throwUnexpectedState() {
+            throw "Unexpected parser state: " + state;
+        }
+    }
+
+    return {
+        parseString: parseString
+    };
+
+})();

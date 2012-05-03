de69fa89c2102dd042b0bb51c30a5525d390ee53
Roberto Rossi <roberto.rossi@paviasystem.com>
2012-05-03 18:47:40 +0200

    Misc

---
+++ javascript/src/widgets/Menu.js
@@ -0,0 +1,49 @@
+﻿/* JPVS
+Module: widgets
+Classes: Menu
+Depends: core
+*/
+
+jpvs.Menu = function (selector) {
+    this.attach(selector);
+
+    this.click = jpvs.event(this);
+};
+
+
+jpvs.Menu.Templates = {
+
+    HorizontalMenuBar: function (menuItems) {
+    },
+
+    VerticalMenuBar: function (menuItems) {
+    },
+
+    PopupMenu: function (menuItems) {
+    }
+
+};
+
+
+jpvs.makeWidget({
+    widget: jpvs.Menu,
+    type: "Menu",
+    cssClass: "Menu",
+
+    create: function (container) {
+        var obj = document.createElement("div");
+        $(container).append(obj);
+        return obj;
+    },
+
+    init: function (W) {
+    },
+
+    canAttachTo: function (obj) {
+        //No autoattach
+        return false;
+    },
+
+    prototype: {
+    }
+});

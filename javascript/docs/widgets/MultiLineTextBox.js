01ba8fc9f4905d9bf21bedb0331b6f0e3f8695bc
Roberto Rossi <roberto.rossi@paviasystem.com>
2012-06-14 12:22:56 +0200

    Misc

---
+++ javascript/docs/widgets/MultiLineTextBox.js
@@ -0,0 +1,22 @@
+﻿window.jpvs = window.jpvs || {};
+
+jpvs.MultiLineTextBox = function (selector) {
+    /// <summary>Attaches the widget to an existing element.</summary>
+    /// <param name="selector" type="Object">Where to attach the widget: jpvs widget or jQuery selector or jQuery object or DOM element.</param>
+
+    this.change = jpvs.event(this);
+};
+
+jpvs.makeWidget({
+    widget: jpvs.MultiLineTextBox,
+    type: "MultiLineTextBox",
+
+    prototype: {
+        text: function (value) {
+            /// <summary>Property: text of the multi-line textbox.</summary>
+            return this;
+        }
+    }
+});
+
+
---
+++ javascript/src/widgets/MultiLineTextBox.js
@@ -0,0 +1,42 @@
+﻿/* JPVS
+Module: widgets
+Classes: MultiLineTextBox
+Depends: core
+*/
+
+jpvs.MultiLineTextBox = function (selector) {
+    this.attach(selector);
+
+    this.change = jpvs.event(this);
+};
+
+jpvs.makeWidget({
+    widget: jpvs.MultiLineTextBox,
+    type: "MultiLineTextBox",
+    cssClass: "MultiLineTextBox",
+
+    create: function (container) {
+        var obj = document.createElement("textarea");
+        $(container).append(obj);
+        return obj;
+    },
+
+    init: function (W) {
+        this.element.change(function () {
+            return W.change.fire(W);
+        });
+    },
+
+    canAttachTo: function (obj) {
+        return $(obj).is("textarea");
+    },
+
+    prototype: {
+        text: jpvs.property({
+            get: function () { return this.element.val(); },
+            set: function (value) { this.element.val(value); }
+        })
+    }
+});
+
+

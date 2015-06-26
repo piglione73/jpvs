32a4d90f9335d2f02001b1c91a00cc847a2eb9c3
piglione73 <roberto.rossi@paviasystem.com>
2015-06-26 12:29:49 +0200

    Lazy tasks

--- build/jpvs-all.js
+++ build/jpvs-all.js
@@ -2183,6 +2183,43 @@ jpvs.Event.prototype.fire = function (widget, handlerName, params, browserEvent)
 
 (function () {
 
+    var taskMap = {};
+
+    jpvs.runLazyTask = function (taskID, delayMillisec, taskFunction) {
+        if (!taskFunction)
+            return;
+
+        //If a timer is already running, then clear it
+        jpvs.cancelLazyTask(taskID);
+
+        //Then schedule (or reschedule) after delayMillisec
+        taskMap[taskID] = setTimeout(run, delayMillisec);
+
+        function run() {
+            //Before launching the task function, let's delete the timeout ID
+            delete taskMap[taskID];
+
+            //Run the task
+            if (taskFunction)
+                taskFunction();
+        }
+    };
+
+    jpvs.cancelLazyTask = function (taskID) {
+        //If a timer is already running, then clear it
+        var timeoutID = taskMap[taskID];
+        if (timeoutID !== null && timeoutID !== undefined)
+            clearTimeout(timeoutID);
+
+        delete taskMap[taskID];
+    };
+
+})();
+;
+
+
+(function () {
+
     var chars = "QWERTYUIOPLKJHGFDSAZXCVBNM1234567890";
     var N = chars.length;
     var M = 10 * N;
--- build/jpvs-doc.js
+++ build/jpvs-doc.js
@@ -482,6 +482,25 @@ jpvs.toJSON = function (obj) {
 
 window.jpvs = window.jpvs || {};
 
+
+jpvs.runLazyTask = function (taskID, delayMillisec, taskFunction) {
+    /// <summary>Schedules or reschedules a task to run after "delayMillisec". The task is uniquely identified by the given "taskID".
+    /// The "taskFunction" is scheduled to run "delayMillisec" after calling this function. However, if during the delayMillisec wait period
+    /// this function is called again, then the previously scheduled run of "taskFunction" is canceled and rescheduled "delayMillisec" from now.</summary>
+    /// <param name="taskID" type="String">ID that will be attached to this task.</param>
+    /// <param name="delayMillisec" type="Number">Delay in milliseconds, after which the lazy task will be run.</param>
+    /// <param name="taskFunction" type="Function">Function that represents the task to be run.</param>
+};
+
+jpvs.cancelLazyTask = function (taskID) {
+    /// <summary>Cancels a scheduled lazy task.</summary>
+    /// <param name="taskID" type="String">ID that will be attached to this task.</param>
+};
+;
+
+
+window.jpvs = window.jpvs || {};
+
 jpvs.randomString = function (len) {
     /// <summary>Creates a random string of a given length and containing uppercase letters and digits only.</summary>
     /// <param name="len" type="Number">The length of the string to be generated.</param>

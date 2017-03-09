1d0217a7942dfb1e14d7944306d967966278f100
piglione73 <roberto.rossi@paviasystem.com>
2017-03-09 16:00:33 +0100

    SearchEngine

---
+++ javascript/docs/widgets/SearchEngine.js
@@ -0,0 +1,39 @@
+window.jpvs = window.jpvs || {};
+
+jpvs.SearchEngine = function (selector) {
+    /// <summary>Attaches the widget to an existing element.</summary>
+    /// <param name="selector" type="Object">Where to attach the widget: jpvs widget or jQuery selector or jQuery object or DOM element.</param>
+};
+
+jpvs.makeWidget({
+    widget: jpvs.SearchEngine,
+    type: "SearchEngine",
+
+    prototype: {
+        label: function (value) {
+            /// <summary>Property: label displayed on top of the search string textbox.</summary>
+            return this;
+        },
+
+        searchFunction: function (value) {
+            /// <summary>Property: function that this widget uses for searching whenever the user enters text into
+            /// the textbox. The function must be declared as: function(text, callback) {} and must search for the
+            /// given text. At the end, it must call callback(searchResults) where "searchResults" is the array of items
+            /// to be displayed.</summary>
+            return this;
+        },
+
+        gridTemplate: function (value) {
+            /// <summary>Property: template used for the jpvs.DataGrid that displays the results. Please see the
+            /// documentation of jpvs.DataGrid for information on the "template" property.</summary>
+            return this;
+        },
+
+        pageSize: function (value) {
+            /// <summary>Property: page size for grid pagination. If null, no pagination is applied. Otherwise, a pager is
+            /// displayed on top of the DataGrid.</summary>
+            return this;
+        }
+    }
+});
+
--- javascript/libs/jpvs-all.js
+++ javascript/libs/jpvs-all.js
@@ -11062,6 +11062,110 @@ jpvs.makeWidget({
 
 (function () {
 
+    jpvs.SearchEngine = function (selector) {
+        this.attach(selector);
+    };
+
+    jpvs.makeWidget({
+        widget: jpvs.SearchEngine,
+        type: "SearchEngine",
+        cssClass: "SearchEngine",
+
+        create: function (container) {
+            var obj = document.createElement("div");
+            $(container).append(obj);
+            return obj;
+        },
+
+        init: function (W) {
+            W.lbl = jpvs.writeTag(W, "div", "Missing 'label' property.").addClass("Label");
+
+            W.txt = jpvs.TextBox.create(W);
+            W.txt.lazychange(onLazyChangeText(W));
+
+            W.pnl = jpvs.writeTag(W, "div").addClass("Results");
+            W.grid = jpvs.DataGrid.create(W.pnl);
+            W.grid.enableEvenOdd(true);
+            W.grid.element.addClass("Grid");
+        },
+
+        canAttachTo: function (obj) {
+            return false;
+        },
+
+        focus: function () {
+            this.txt.focus();
+        },
+
+        prototype: {
+            label: jpvs.property({
+                get: function () { return this.lbl.text(); },
+                set: function (value) { this.lbl.text(value); }
+            }),
+
+            searchFunction: jpvs.property({
+                get: function () {
+                    return this.element.data("searchFunction") || function () {
+                        alert("Missing 'searchFunction' property.");
+                    };
+                },
+                set: function (value) {
+                    this.element.data("searchFunction", value);
+                }
+            }),
+
+            gridTemplate: jpvs.property({
+                get: function () {
+                    return this.element.data("gridTemplate") || [{
+                        header: "Missing 'gridTemplate' property.",
+                        body: "Missing 'gridTemplate' property."
+                    }];
+                },
+                set: function (value) {
+                    this.element.data("gridTemplate", value);
+                }
+            }),
+
+            pageSize: jpvs.property({
+                get: function () {
+                    return this.element.data("pageSize") || null;
+                },
+                set: function (value) {
+                    this.element.data("pageSize", value);
+                }
+            })
+        }
+    });
+
+    function onLazyChangeText(W) {
+        return function () {
+            //If no search text, then hide the results grid
+            if (!W.txt.text() || !$.trim(W.txt.text()))
+                W.pnl.hide();
+
+            //Whenever the user entered something into the textbox, we call a function to get some data to display
+            var functionToCall = W.searchFunction();
+            functionToCall(W.txt.text(), function (returnedItems) {
+                //If no results, then hide the results grid
+                if (!returnedItems || !returnedItems.length) {
+                    W.pnl.hide();
+                    return;
+                }
+
+                //If we have results, then let's display them
+                W.grid.clear().template(W.gridTemplate());
+                W.grid.binder(W.pageSize() ? jpvs.DataGrid.pagingBinder({ pageSize: W.pageSize() }) : jpvs.DataGrid.defaultBinder);
+                W.grid.addHeaderRow({}).dataBind(returnedItems);
+                W.pnl.show();
+            });
+        };
+    }
+})();
+;
+
+
+(function () {
+
     jpvs.Table = function (selector) {
         this.attach(selector);
     };

c45772e8e9d1c9b313ca5175a1505e4509e44fac
piglione73 <roberto.rossi@paviasystem.com>
2017-12-18 18:04:41 +0100

    Filtering utility did not handle numbers properly

--- javascript/src/utils/Filtering.js
+++ javascript/src/utils/Filtering.js
@@ -10,10 +10,10 @@
             var failure = false;
             for (var ruleIndex = 0; ruleIndex < filteringRules.length; ruleIndex++) {
                 var rule = filteringRules[ruleIndex];
-                var leftString = (rule.selector(item) || "").toString().toUpperCase();
-                var rightString = (rule.value || "").toString().toUpperCase();
+                var leftValue = rule.selector(item);
+                var rightValue = rule.value;
                 var operand = rule.operand.toUpperCase();
-                var ok = isRuleOk(leftString, operand, rightString);
+                var ok = isRuleOk(leftValue, operand, rightValue);
                 if (!ok) {
                     failure = true;
                     break;
@@ -29,45 +29,68 @@
     };
 
     function isRuleOk(left, operand, right) {
+        //A missing value never satisfies
+        if (left === undefined || left === null)
+            return false;
+
+        var leftNumber = (typeof (left) == "number");
+        var rightNumber = (right !== undefined && right !== null && (typeof (right) == "number" || isFinite(parseFloat(right))));
+        var useNumbers = (leftNumber && rightNumber);
+
+        var leftString = (left || "").toString().toUpperCase();
+        var rightString = (right || "").toString().toUpperCase();
+
         if (operand == "EQ") {
             //Equals
-            return left == right;
+            return leftString == rightString;
         }
         else if (operand == "NEQ") {
             //Not equals
-            return left != right;
+            return leftString != rightString;
         }
         else if (operand == "CONTAINS") {
             //Left contains right
-            return left.indexOf(right) >= 0;
+            return leftString.indexOf(rightString) >= 0;
         }
         else if (operand == "NCONTAINS") {
             //Left does not contain right
-            return left.indexOf(right) < 0;
+            return leftString.indexOf(rightString) < 0;
         }
         else if (operand == "STARTS") {
             //Left starts with right
-            return left.indexOf(right) == 0;
+            return leftString.indexOf(rightString) == 0;
         }
         else if (operand == "NSTARTS") {
             //Left does not start with right
-            return left.indexOf(right) != 0;
+            return leftString.indexOf(rightString) != 0;
         }
         else if (operand == "LT") {
-            //left < right
-            return left < right;
+            //left < right, numerically if possible
+            if (useNumbers)
+                return left < parseFloat(right);
+            else
+                return leftString < rightString;
         }
         else if (operand == "LTE") {
-            //left <= right
-            return left <= right;
+            //left <= right, numerically if possible
+            if (useNumbers)
+                return left <= parseFloat(right);
+            else
+                return leftString <= rightString;
         }
         else if (operand == "GT") {
-            //left > right
-            return left > right;
+            //left > right, numerically if possible
+            if (useNumbers)
+                return left > parseFloat(right);
+            else
+                return leftString > rightString;
         }
         else if (operand == "GTE") {
-            //left >= right
-            return left >= right;
+            //left >= right, numerically if possible
+            if (useNumbers)
+                return left >= parseFloat(right);
+            else
+                return leftString >= rightString;
         }
         else {
             //Unknown rule: assume false
